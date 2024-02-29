ae38416b0ee318aea5a1f8ed62734954157c7bdf
abhay-on-it <abhayagnihotri1585@gmail.com>
2024-02-29 16:33:02 +0530

    added the main marquee

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import  Navbar  from "./components/Navbar";
 import  Page1 from "./components/pg1"
+import Marquee from './components/Marquee';
 
 function App() {
   return (
-    <div className='w-full h-screen'>
+    <div className='w-full min-h-dvh bg-slate-200'>
       <Navbar/>
       <Page1 />
+      <Marquee/>
     </div>
   )
 }
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Marquee() {
   return (
-    <div>
-        
+    <div className=' bg-[#004D43] flex items-center rounded-tl-3xl rounded-tr-3xl  text-white w-full py-20'>
+        <div className="marquee-in flex items-center justify-between overflow-hidden border-t-2 border-b border-slate-200">
+            <h1 className=' uppercase pb-12 boredr font-["founders"] text-[30vmax] whitespace-nowrap leading-[20vmax]'>We Are Ochi</h1>
+            <h1 className='uppercase pb-12 boredr font-["founders"] text-[30vmax] whitespace-nowrap leading-[20vmax]'>We Are Ochi</h1>
+        </div>
     </div>
   )
 }
